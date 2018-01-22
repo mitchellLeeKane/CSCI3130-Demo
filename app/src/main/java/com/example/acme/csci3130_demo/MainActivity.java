46a34ce65eed13170063f9c724824c97b5eca632
MitchellLeeKane <mkane@dal.ca>
2018-01-22 12:39:53 -0400

    Implemented Button
    Added a button that updates text field to whatever was typed in text block.

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.*;
+import android.widget.*;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void updateText(View myView){
+        TextView starting = (TextView) findViewById(R.id.textView);
+        EditText updated = (EditText) findViewById(R.id.editText);
+        starting.setText(updated.getText().toString());
     }
 }
